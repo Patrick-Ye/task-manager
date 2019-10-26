6b2971e4877b304231553012bd8e20e9ed143275
Peng Ye <yep0608@hotmail.com>
2019-10-26 20:35:19 +1000

    26/10

--- mongodb.js
+++ mongodb.js
@@ -11,8 +11,49 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  db.collection('users').insertOne({
-    name: 'Peng',
-    age: 27
+  // db.collection('users').insertOne({
+  //   name: 'Peng',
+  //   age: 27
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user')
+  //   }
+
+  //   console.log(result.ops)
+  // })
+  
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Jack',
+  //     age: 24
+  //   }, {
+  //     name: 'Rose',
+  //     age: 23
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert users')
+  //   }
+
+  //   console.log(result.ops)
+  // })
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'A',
+      completed: true
+    }, {
+      description: 'B',
+      completed: false
+    }, {
+      description: 'C',
+      completed: true
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('Unable to insert tasks')
+    }
+
+    console.log(result.ops)
   })
 })
